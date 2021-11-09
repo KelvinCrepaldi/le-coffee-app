1fbd17627948f41d6133e9717dc544660128e579
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-11-09 18:24:48 -0300

    feat: created login page

---
+++ src/Pages/Login/index.jsx
@@ -0,0 +1,15 @@
+import logo from './Logo-main-black-4.png';
+const LoginPage = () => {
+  return (
+    <div style={{background: "#504A59"}}>
+    <img src={logo} alt="Mug logo" />
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+    <div>
+      <h1>Bem vindo</h1>
+        
+    </div>
+    </div>
+  );
+};
+
+export default LoginPage;
