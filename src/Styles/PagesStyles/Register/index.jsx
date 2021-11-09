23009d93568404b684e37b9fc51fbb5dd54f9bec
Kelvin Crepaldi <kelvin.creps@hotmail.com>
2021-11-09 20:36:25 -0300

    feat: page register updated

--- src/Styles/PagesStyles/Register/index.jsx
+++ src/Styles/PagesStyles/Register/index.jsx
@@ -2,18 +2,19 @@ import styled from "styled-components";
 
 export const Page = styled.div`
   display: flex;
-  flex-direction: column;
+  justify-content: center;
 
   min-width: 320px;
   min-height: 100vh;
 
-  background-color: var(--white-primary);
+  background-color: #f5f5f5;
 `;
 
 export const PageContent = styled.main`
   display: flex;
-  flex-direction: column;
+  flex-wrap: wrap;
   align-items: center;
+  align-content: space-between;
 
   width: 270px;
 
@@ -28,9 +29,40 @@ export const PageContent = styled.main`
     font-size: var(--font-size-small);
     color: #212621;
   }
+  form {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  input {
+    margin: 7px;
+    width: 100%;
+    height: 45px;
+  }
 
   .title {
     width: 176px;
     text-align: center;
+    margin: 22px auto 19px;
+  }
+
+  .button-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    width: 100%;
+    height: 135px;
+    margin-bottom: 30px;
+  }
+
+  .text-register {
+    display: flex;
+    align-items: center;
+
+    font-size: var(--font-size-xsmall);
+    font-weight: var(--font-w-normal);
+    height: 29px;
   }
 `;
