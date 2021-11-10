406a72ded1723eaaf31fcef97e28e708eb73d80c
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-11-10 10:13:50 -0300

    feat: desktop UI

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,9 +1,22 @@
 import { LoginForm } from "../../Components/LoginForm";
 import { Page } from "../../Styles/PagesStyles/Login";
 import logo from "./Logo-main-black-4.png";
+import bgImageLeft from "./cover-rodape.png";
+import bgImageRight from "./about-us-cover-2.png";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 const LoginPage = () => {
   return (
     <Page>
+      <div className="bgContent">
+        <img className="imgLeft" src={bgImageLeft} alt="Mug logo" />
+        <img className="imgRight" src={bgImageRight} alt="Mug logo" />
+      </div>
+      <div className="toLogin">
+        <button>
+          <AiOutlineArrowLeft className="icon"/>
+        </button>
+        <p>Cadastro</p>
+      </div>
       <div className="topContent">
         <img src={logo} alt="Mug logo" />
       </div>
