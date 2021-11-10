385316744dbac0b4fc86f932e52e1401be542eb9
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-11-09 21:48:12 -0300

    feat: created login form component

---
+++ src/Components/LoginForm/index.jsx
@@ -0,0 +1,52 @@
+import { Input } from "../Input";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { LoginFormContainer } from "../../Styles/ComponentsStyle/LoginForm";
+
+export const LoginForm = () => {
+  const loginSchema = yup.object({
+    email: yup.string().required("Campo obrigatório").email("Email inválido"),
+    password: yup.string().required().min(6, "mínimo de 6 caracteres"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(loginSchema),
+  });
+
+  const login = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <LoginFormContainer onSubmit={handleSubmit(login)}>
+      <Input
+        placeholder="Email"
+        register={register}
+        name="email"
+        error={errors.email?.message}
+      />
+
+      <Input
+        placeholder="Senha"
+        register={register}
+        name="password"
+        error={errors.password?.message}
+      />
+      
+      <div className="bttnContainer">
+        <button className="bttnLogin" type="submit">
+          Entrar
+        </button>
+        <button className="bttnRegister">
+          Registrar-se
+        </button>
+      </div>
+
+    </LoginFormContainer>
+  );
+};
--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,3 +1,4 @@
+import { LoginForm } from '../../Components/LoginForm';
 import { Page } from '../../Styles/PagesStyles/Login';
 import logo from './Logo-main-black-4.png';
 const LoginPage = () => {
@@ -7,6 +8,7 @@ const LoginPage = () => {
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
     <div>
       <h1>Bem vindo</h1>
+      <LoginForm/>
     </div>
     </Page>
   );
