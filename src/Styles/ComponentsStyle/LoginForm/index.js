2ca711ee3551a18ecaf0c18adf36af4a33d01945
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-11-09 22:02:53 -0300

    style: form layout

--- src/Styles/ComponentsStyle/LoginForm/index.js
+++ src/Styles/ComponentsStyle/LoginForm/index.js
@@ -7,8 +7,33 @@ export const LoginFormContainer = styled.form`
   justify-content: flex-start;
   line-height: 40px;
 
-  .bttnLogin {
-  }
-  .bttnRegister {
+  .bttnContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    
+    width: 78%;
+    margin-top: 60px;
+    .bttnLogin {
+      height: 50px;
+      width: 78%;
+
+      background-color: #aa6655;
+      color: #fbfbfb;
+      border: none;
+      border-radius: 17px;
+      margin-bottom: 20px;
+    }
+
+    .bttnRegister {
+      height: 40px;
+      width: 78%;
+
+      background-color: #ffffff;
+      color: #808080;
+      border: none;
+      border-radius: 17px;
+    }
   }
 `;
