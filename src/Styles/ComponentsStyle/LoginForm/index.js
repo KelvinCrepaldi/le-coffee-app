ce3d10c7d44d7a737451985a84783250199a99b5
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-11-09 21:48:37 -0300

    style: form layout

---
+++ src/Styles/ComponentsStyle/LoginForm/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const LoginFormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  line-height: 40px;
+
+  .bttnLogin {
+  }
+  .bttnRegister {
+  }
+`;
