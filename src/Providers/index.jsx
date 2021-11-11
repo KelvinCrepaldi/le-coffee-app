ec3c69770d63309b22a640d12afc67528c246dfe
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-11-11 16:03:23 -0300

    feat: userProvider

--- src/Providers/index.jsx
+++ src/Providers/index.jsx
@@ -1,10 +1,16 @@
 import { RegisterProvider } from "./registerProvider";
 import { LoginProvider } from "./loginProvider";
+import { UserProvider } from "./userProvider";
+
 const ProvidersGathered = ({ children }) => {
   return (
     <>
       <RegisterProvider>
-        <LoginProvider>{children}</LoginProvider>
+        <LoginProvider>
+          <UserProvider>
+          {children}
+          </UserProvider>
+        </LoginProvider>
       </RegisterProvider>
     </>
   );
