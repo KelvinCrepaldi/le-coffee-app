5cf064f11c2f3e671a1df5c83ac6ef87389f376e
Roberto Rocha <rsantos.rocha@gmail.com>
2021-11-12 09:33:45 -0300

    feat: implementation of the Provider Comments

---
+++ src/Providers/comments/index.jsx
@@ -0,0 +1,43 @@
+import { createContext, useState } from "react";
+import api from "../../Services";
+
+export const CommentContext = createContext();
+
+export const CommentProvider = ({ children }) => {
+  const [rating, setRating] = useState([]);
+  
+  const getRatingProducts = () => {
+    api.get("ratingProducts")
+        .then((response) => {
+            console.log(response.data);
+            setRating(response.data);
+        })
+        .catch((error) => console.log(error))
+  };
+
+  const handleRating = (data, token) => {
+      api.post("ratingProducts", data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+          console.log(response.data);
+      })
+      .catch((error) => console.log(error));
+  }
+
+  const deleteRating =  (ratingId) => {
+    api.delete(`ratingProducts/${ratingId}`, {
+      headers: {
+        Authorization: `Bearer ${usrToken}`,
+      },
+    })
+    .then(console.log("deleted"))
+    .catch((error) => console.log(error));
+  }
+
+  return (
+    <CommentContext.Provider value={{ getRatingProducts, handleRating, deleteRating }}>{children}</CommentContext.Provider>
+  );
+};
