0a16dc699d58fffcb084f75e9143c107d7c93be1
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-11-16 15:18:58 -0300

    feat: add or remove item on cart card

--- src/Components/CartProduct/index.jsx
+++ src/Components/CartProduct/index.jsx
@@ -1,8 +1,9 @@
 import { CartCard } from "../../Styles/ComponentsStyle/CartProduct";
 import { ButtonComponent } from "../Button";
 import { useContext, useState } from "react";
-import { UserContext } from "../../Providers/userProvider"
-export const CartProduct = ({product}) => {
+import { UserContext } from "../../Providers/userProvider";
+import { AddRemovePdt } from "../../Styles/ComponentsStyle/CartProduct";
+export const CartProduct = ({ product }) => {
   const [qtd, setQtd] = useState(0);
   const [userToken] = useState(() => {
     const current = localStorage.getItem("token") || "";
@@ -13,16 +14,29 @@ export const CartProduct = ({product}) => {
 
   const handleRemove = () => {
     removeFromCart(product.id, userToken);
-  }
+  };
 
-  return(
-      <CartCard>
-          <img src={product.image} alt="coffee" />
-          <div className="info">
-            <p>{product.name}</p>
-            <p className="price">R$: {product.price}</p>
-          </div>
-          <ButtonComponent variant="brown" text="Remover" onClick={handleRemove}/>
-      </CartCard>
-  )
-}
+  return (
+    <CartCard>
+      <img src={product.image} alt="coffee" />
+      <div className="info">
+        <p>{product.name}</p>
+        <p className="price">R$: {product.price}</p>
+      </div>
+
+      <AddRemovePdt>
+        <button className="sub-add">
+          -
+        </button>
+        <div className="qtd">
+          <p>{product.quantity}</p>
+        </div>
+        <button className="sub-add">
+          +
+        </button>
+      </AddRemovePdt>
+
+      <ButtonComponent variant="brown" text="Remover" onClick={handleRemove} />
+    </CartCard>
+  );
+};
