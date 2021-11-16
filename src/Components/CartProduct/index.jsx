0c7042b8e0e5d197e04633d038fc1f8371a19e6b
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-11-16 19:05:35 -0300

    feat: updating product quantity and the final price

--- src/Components/CartProduct/index.jsx
+++ src/Components/CartProduct/index.jsx
@@ -3,8 +3,9 @@ import { ButtonComponent } from "../Button";
 import { useContext, useState } from "react";
 import { UserContext } from "../../Providers/userProvider";
 import { AddRemovePdt } from "../../Styles/ComponentsStyle/CartProduct";
+import api from "../../Services";
+
 export const CartProduct = ({ product }) => {
-  const [qtd, setQtd] = useState(0);
   const [userToken] = useState(() => {
     const current = localStorage.getItem("token") || "";
     return JSON.parse(current);
@@ -16,6 +17,24 @@ export const CartProduct = ({ product }) => {
     removeFromCart(product.id, userToken);
   };
 
+  const removeOnePdt = () => {
+    const newQtd = product.quantity - 1
+    api.patch(`userCart/${product.id}`, { quantity: newQtd }, {
+      headers: {
+        Authorization: `Bearer ${userToken}`
+      }
+    })
+  }
+  
+  const addOnePdt = () => {
+    const newQtd = product.quantity + 1
+    api.patch(`userCart/${product.id}`, { quantity: newQtd }, {
+      headers: {
+        Authorization: `Bearer ${userToken}`
+      }
+    })
+  }
+
   return (
     <CartCard>
       <img src={product.image} alt="coffee" />
@@ -25,13 +44,13 @@ export const CartProduct = ({ product }) => {
       </div>
 
       <AddRemovePdt>
-        <button className="sub-add">
+        <button className="sub-add" onClick={removeOnePdt}>
           -
         </button>
         <div className="qtd">
           <p>{product.quantity}</p>
         </div>
-        <button className="sub-add">
+        <button className="sub-add" onClick={addOnePdt}>
           +
         </button>
       </AddRemovePdt>
