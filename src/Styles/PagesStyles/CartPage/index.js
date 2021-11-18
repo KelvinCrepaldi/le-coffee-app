32d6695f7f2800d2d23a5435400a12ba1cd77e30
Leonam Rodrigues <leonam_5000@hotmail.com>
2021-11-18 10:12:37 -0300

    feature(component/modal): add ordered modal

--- src/Styles/PagesStyles/CartPage/index.js
+++ src/Styles/PagesStyles/CartPage/index.js
@@ -7,7 +7,11 @@ export const Page = styled.div`
   justify-content: flex-start;
 
   min-height: 100vh;
-
+ 
+#close {
+  color: red;
+  width: 100px;
+}
   .navBar {
     display: flex;
     align-items: center;
@@ -112,3 +116,79 @@ export const CardTop = styled.div`
     font-weight: var(--font-w-bold);
   }
 `;
+
+export const FormModal = styled.form`
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  div{
+    width: 100%;
+  }
+  input {
+    width: 100%;
+  }
+ 
+  h2 {
+    margin: 20px;
+    color: var(--gray-primary)
+  }
+  p {
+    width: 90%;
+    margin: 0 auto;
+    text-align: center;
+    color: #636e72
+  }
+  button{
+    width: 100%;
+    border-radius: 0px;
+    padding: 20px;
+    color:#61db61;
+    margin-top: 20px;
+
+    :hover{
+      opacity: 0.8;
+    }
+  }
+
+
+`
+
+export const OrderSuccess = styled.div`
+
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+
+  h2 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 26px;
+
+    text-align: center;
+    color: var(--gray-primary);
+    flex-wrap: wrap;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+   
+    margin-top: 50px;
+    width: 69%;
+    p{
+      color: var(--gray-primary);
+    }
+    svg {
+      font-size: 96px;
+      color:#61db61;;
+      margin: 40px;
+    }
+  }
+`
